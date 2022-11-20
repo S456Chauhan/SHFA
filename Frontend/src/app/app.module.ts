db4b6b5e01c5879632719464c4f909ddc10ec2cc
Sahil45Chauhan <sahilchauhan9898@gmail.com>
2022-11-20 22:55:28 +0530

    services

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HousingService } from './services/housing.service';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +18,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     NavBarComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    HousingService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
